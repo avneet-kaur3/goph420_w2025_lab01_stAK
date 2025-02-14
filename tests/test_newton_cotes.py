e069fd46d2488ab5bd9c3241d6a80e2be80a9dfb
Avneet Kaur <avneet.kaur3@ucalgary.ca>
2025-02-14 15:03:41 -0700

    added TestNewton functions to test trapezoid rule and simpsons rule

---
+++ tests/test_newton_cotes.py
@@ -0,0 +1,29 @@
+import numpy as np
+import unittest
+import matplotlib.pyplot as plt
+from integrate import integrate_newton
+
+class TestNewton(unittest.TestCase):
+
+
+    def test_trapezoidal(self):
+        x = np.linspace(0, 10, 101)
+        f = (5*x) + 10
+        calculated = integrate_newton(x, f, 'trap')
+        print(f'Trapezoidal result: {calculated}')
+
+    def test_simpsons_even(self):
+        x = np.linspace(0, 10, 100)
+        f = (5*(x**2)) + x + 10
+        calculated = integrate_newton(x, f, 'simp')
+        print(f'Simpson\'s result with even number of data points: {calculated}')
+
+    def test_simpsons_odd(self):
+        x = np.linspace(0, 10, 101)
+        f = (5*(x**2)) + x + 10
+        calculated = integrate_newton(x, f, 'simp')
+        print(f'Simpson\'s result with odd number of data points: {calculated}')
+
+if __name__=="__main__":
+    unittest.main()
+
