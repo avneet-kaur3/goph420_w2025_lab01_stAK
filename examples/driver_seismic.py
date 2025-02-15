9a19b23436efda4b4fab7b73958b85016e62f57a
Avneet Kaur <avneet.kaur3@ucalgary.ca>
2025-02-14 18:40:58 -0700

    debug

--- examples/driver_seismic.py
+++ examples/driver_seismic.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from src.goph420_lab01.integration import integrate_newton 
+
+from goph420_lab01.integration import (
+    integrate_newton,
+    )
 
 def main():
     #Loading seismic data.
