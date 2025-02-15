42afcce68449a1e9d1e6474769c191c58b955096
Avneet Kaur <avneet.kaur3@ucalgary.ca>
2025-02-14 17:06:02 -0700

    added integrate_gauss function to the file

--- src/goph420_lab01/integration.py
+++ src/goph420_lab01/integration.py
@@ -2,9 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def integrate_newton(x, f, alg):
+def integrate_newton(x, f, alg = 'trap'):
     '''Approximates the integral of f(x) over a set time period from 0 to 10 seconds by Newton-Cotes rules.
     The function can carry out the integration through the trapezoid rule or the 1/3 Simpson's rule.
+    The default value for the alg flag set is 'trap'.
 
     Parameters: 
         x (array_like) : Coordinates of the sample points.
@@ -23,8 +24,10 @@ def integrate_newton(x, f, alg):
     if (x.ndim != 1) or (f.ndim != 1):
         raise ValueError('Arrays x and f must be one-dimensional.')
 
-    dx = 0.01
+    #Defining the step size of the coordinates by taking the difference between the first two.
+    dx = x[1] - x[0]
 
+    #Placing conditional statements based whether the chosen algorithm is trapezoid rule or simpson's 1/3 rule.
     if(alg == 'trap'):
         integral = (dx/2) * np.sum(f[1:] + f[:-1])
 
@@ -32,3 +35,45 @@ def integrate_newton(x, f, alg):
         integral = (dx/3) * (f[0] + f[-1] + (4 * np.sum(f[1:-1:2])) + (2 * np.sum(f[2:-2:2])))
 
     return integral
+
+
+def integrate_gauss(f, lims, npts = 3):
+    '''Approximates the integral of f(x) over a set time period from 0 to 10 seconds by Newton-Cotes rules.
+    The function can carry out the integration through the trapezoid rule or the 1/3 Simpson's rule.
+    The default value for the alg flag set is 'trap'.
+
+    Parameters: 
+        f : Reference to a callable object.
+        lims (object) : Lower and upper bounds of the integration.
+        npts (integer) : Number of integration points to us.
+    Returns: 
+        integral (float) : Provides a floating point value for the integral estimate.
+    '''
+
+    #If f is not callable, a TypeError will be raised.
+    if not callable(f):
+        raise TypeError('Parameter f must be a callable function.')
+
+    #If the length of lims is not 2, a ValueError will be raised.
+    if (len(lims) != 2):
+        raise ValueError("The lims parameter requires a length of two - upper and lower parameters.")
+
+    #If the upper and lower limits of the integral cannot be converted to float, a ValueError will be raised.
+    try:
+        float(lims[0]), float(lims[1])
+    except ValueError:
+        raise ValueError("The lims parameter must have values that are convertible to float.")
+
+    #If npts is not in [1, 2, 3, 4, 5], a ValueError will be raised.
+    if npts not in [1, 2, 3, 4, 5]:
+        raise ValueError('Parameter npts must be in [1, 2, 3, 4, 5].')
+
+    #Getting the sample points and weights for the Gauss-Legendre quadrature.
+    x_k, w = np.polynomial.legendre.legauss(npts)
+    #The function above only works for an interval of [-1, 1]. Values are translated to the actual limits.
+    x = 0.5*(x_k + 1)*(lims[1] - lims[0]) + lims[0]
+    #Calculating the integral.
+    integral = np.sum(w * f(x)) * 0.5 * (lims[1] - lims[0])
+
+    return integral
+
