597f05c433c061492a9344b06a8c032f47fcca18
Avneet Kaur <avneet.kaur3@ucalgary.ca>
2025-02-24 20:30:15 -0700

    added comments to document the test functions

--- tests/test_newton_cotes.py
+++ tests/test_newton_cotes.py
@@ -6,6 +6,11 @@ from src.goph420_lab01.integration import integrate_newton
 
 class TestNewton(unittest.TestCase):
 
+    '''
+    The following functions perform tests on the integrate_newton function imported from the file integration.py. It tests the Trapezoidal Rule and Simpson's
+    1/3 Rule algorithms with a linear function for the Trapezoidal Rule and a quadratic function for the Simpson's Rule. The tests for the Simpson's Rule also consider
+    an odd number of data points and an even number of data points.
+    '''
 
     def test_trapezoidal(self):
         x = np.linspace(0, 10, 101)
